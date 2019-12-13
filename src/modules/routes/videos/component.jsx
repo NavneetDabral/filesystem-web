15726166827bc4a7012f5347fc5ac11adb572764
Navneet Dabral <dabralnavneet@gmail.com>
2019-12-13 13:11:09 +0530

    Add files via upload

---
+++ src/modules/routes/videos/component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let Component =()=>{
+    return(
+        <div className='sweet-loading loader-overlay'>
+        Hey video
+    </div>  
+    )
+}
+
+export default Component;
---
+++ src/modules/routes/videos/container.jsx
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux';
+import Component from './component'
+
+const mapStateToProps=(state)=>({
+      
+
+})
+
+
+const mapDispatchToProps =(dispatch)=>({
+
+
+
+})
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(Component);
