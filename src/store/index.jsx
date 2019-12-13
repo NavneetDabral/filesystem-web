a3efeae9dcbf1c5e781339e4d80a29b2b00305cc
Navneet Dabral <dabralnavneet@gmail.com>
2019-12-13 13:00:46 +0530

    Add files via upload

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,1 +1,28 @@
+import { createStore, applyMiddleware } from 'redux';
+import { createBrowserHistory } from 'history';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { routerMiddleware } from 'connected-react-router'
+import rootReducer from '../reducers/rootReducer';
+import thunk from 'redux-thunk';
+
+
+
+export const history=createBrowserHistory();
+ console.log(history)
+
+export default function configureStore(initialState){
+   
+  const store = createStore(
+    rootReducer(history), 
+          initialState,
+      composeWithDevTools(
+        applyMiddleware(thunk ,
+          routerMiddleware(history),
+      // other store enhancers if any
+      )
+    )
+  );
+  
+return store;
+}
 
