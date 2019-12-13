2251cb74d31ece99b56178985eaf249d21abb6bd
Navneet Dabral <dabralnavneet@gmail.com>
2019-12-13 13:10:03 +0530

    Add files via upload

---
+++ src/modules/routes/pictures/component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let Component =()=>{
+    return(
+        <div className='sweet-loading loader-overlay'>
+        Hey hi
+    </div> 
+    )
+}
+
+export default Component;
