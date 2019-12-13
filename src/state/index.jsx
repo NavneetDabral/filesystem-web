ebddee8430bf04fec57ea4f82799d7bf4f086909
Navneet Dabral <dabralnavneet@gmail.com>
2019-12-13 13:04:05 +0530

    Add files via upload

--- src/state/index.jsx
+++ src/state/index.jsx
@@ -1,1 +1,38 @@
-
+export  const initialState={
+    appReducer:{
+        application:{
+          files:[
+             {
+                Name:"index.html",
+                Size:"542kb",
+                Creator:"Navneet",
+                Date:"Dec 13,2019",
+                type:"file"
+             },
+             {
+                Name:"index.js",
+                Size:"542kb",
+                Creator:"Navneet",
+                Date:"Dec 13,2019",
+                type:"file"
+             },
+          ],
+          folders:[
+            {
+                Name:"Assets",
+                Size:"542kb",
+                Creator:"Navneet",
+                Date:"Dec 13,2019",
+                type:"folder"
+             },
+             {
+                Name:"Config",
+                Size:"542kb",
+                Creator:"Navneet",
+                Date:"Dec 13,2019",
+                type:"folder"
+             },
+          ] 
+        }
+    }
+}
